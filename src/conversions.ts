6ba0a83556b0abfc61603b459ee802707c089ecb
Ruby20-50 <rubamogalli@gmail.com>
2026-04-29 08:52:33 +0200

    implementing class conversions

--- src/conversions.ts
+++ src/conversions.ts
@@ -1,11 +1,16 @@
 export class Conversions {
     static toNumber(x: any): number {
-        throw new Error("Method not implemented");
+        if(x === null) throw new Error("value can't be undefined");
+        return Number(x)
+        
     }
     static toBoolean(x: any): boolean {
-        throw new Error("Method not implemented");
+          if(x === null) throw new Error("value can't be undefined");
+        if(x === "false") return false;
+        return Boolean([])
     }
     static toString(x: any): string {
-        throw new Error("Method not implemented");
+          if(x === null) throw new Error("value can't be undefined");
+        return String(x);
     }
 }
