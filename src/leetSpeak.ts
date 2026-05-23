15c6e2c0f0b675a3e681e75b58d4de164a7455cf
Ruby20-50 <rubamogalli@gmail.com>
2026-05-23 13:31:50 +0200

    two tests implemented

--- src/leetSpeak.ts
+++ src/leetSpeak.ts
@@ -5,7 +5,7 @@
  * @param letter must have only one character
  */
 export function leetSpeakSingleLetter(letter : String) : String{
-    throw new Error("Function not implemented");
     if(letter.length > 1 )
         throw new Error("this function recieves only one character! ")
+    return letter;
 }
--- tests/leetSpeek.test.ts
+++ tests/leetSpeek.test.ts
@@ -3,3 +3,7 @@ import { leetSpeakSingleLetter } from "../src/leetSpeak";
 test("test leetSpeakSingleLetter thorws error for strings longer than 1", ()=>{
     expect(()=> leetSpeakSingleLetter("hi")).toThrow("this function recieves only one character!");
 })
+
+test("test leetSpeakSingleLetter returns the same letter", ()=>{
+    expect(leetSpeakSingleLetter("l")).toBe("l")
+})
