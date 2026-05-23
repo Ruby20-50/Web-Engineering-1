23c19c5c58510a8e6c4566ccb9b218a3d9ff9799
Ruby20-50 <rubamogalli@gmail.com>
2026-05-23 14:40:59 +0200

    iritative variant Done

--- src/leetSpeak.ts
+++ src/leetSpeak.ts
@@ -4,8 +4,36 @@
  * 
  * @param letter must have only one character
  */
-export function leetSpeakSingleLetter(letter : String) : String{
+export function leetSpeakSingleLetter(letter : string) : string{
     if(letter.length > 1 )
-        throw new Error("this function recieves only one character! ")
-    return letter;
+        throw new Error("this function receives only one character! ")
+    if(letter === 'a' || letter === 'A')
+        return '4';
+    if(letter === 'e' || letter === 'E')
+        return '3';
+    if(letter === 'i' || letter === 'I')
+        return '1';
+    if(letter === 'b' || letter === 'B')
+        return '8';
+    if(letter === 'g' || letter === 'G')
+        return '6';
+    if(letter === 'S' || letter === 's')
+        return '5';
+    if(letter === 'z' || letter === 'Z')
+        return '2';
+    else
+        return letter;
 }
+
+export function leetSpeakIter(input : string) : string{
+    //throw new Error();
+    if(input === " " || input ==="")
+        return input;
+    let leet : string="";
+    for(let i = 0; i < input.length; i++){
+        leet = leet.concat(leetSpeakSingleLetter(input[i]));
+        console.log(leet);
+    }
+    return leet;
+    
+} 
--- tests/leetSpeek.test.ts
+++ tests/leetSpeek.test.ts
@@ -1,9 +1,13 @@
-import { leetSpeakSingleLetter } from "../src/leetSpeak";
+import { leetSpeakIter, leetSpeakSingleLetter } from "../src/leetSpeak";
 
-test("test leetSpeakSingleLetter thorws error for strings longer than 1", ()=>{
-    expect(()=> leetSpeakSingleLetter("hi")).toThrow("this function recieves only one character!");
+test("test leetSpeakSingleLetter throws error for strings longer than 1", ()=>{
+    expect(()=> leetSpeakSingleLetter("hi")).toThrow("this function receives only one character!");
 })
 
 test("test leetSpeakSingleLetter returns the same letter", ()=>{
     expect(leetSpeakSingleLetter("l")).toBe("l")
+})
+
+test("leetSpeakIter", ()=>{
+    expect(leetSpeakIter("Hi")).toBe("H1");
 })
